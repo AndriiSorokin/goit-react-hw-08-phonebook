848c9bbfe524b825e1c4d4937fb0af6dee912320
AndriiSorokin <andrewsorokin07@gmail.com>
2021-03-27 21:04:37 +0200

    make phone book hook

--- src/PhoneBook/PhoneBook.js
+++ src/PhoneBook/PhoneBook.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import Form from './Form';
 import Contacts from './Contacts';
 import { connect } from 'react-redux';
 import Filter from './Filter';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchContact } from '../redux/contactOperations/contactOperations';
 import contactSelector from '../redux/contactSelector/contactSelector';
 import style from '../PhoneBook/PhoneBook.module.css';
 
-class PhoneBook extends Component {
-  state = {
-    contacts: [],
-  };
-  componentDidMount() {
-    this.props.fetchNumber();
-  }
-  render() {
-    const contact = this.props;
-    console.log(contact);
-    return (
-      <div className={style.container}>
-        <h1>PhoneBook</h1>
-        <Form />
-        <Filter />
-        <Contacts />
-      </div>
-    );
-  }
-}
+const PhoneBook = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContact());
+  }, []);
+  return (
+    <div className={style.container}>
+      <h1>PhoneBook</h1>
+      <Form />
+      <Filter />
+      <Contacts />
+    </div>
+  );
+};
+
 const mapDispatchToProps = dispatch => ({
   fetchNumber: () => dispatch(fetchContact()),
 });
 
-const mapStatetoProps = state => ({
-  contact: contactSelector.getVisibleUser(state),
-});
-export default connect(mapStatetoProps, mapDispatchToProps)(PhoneBook);
+export default connect(null, mapDispatchToProps)(PhoneBook);
