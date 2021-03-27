b9c61d1ba99f8542ef016553712b5ae00ca3baf9
AndriiSorokin <andrewsorokin07@gmail.com>
2021-03-27 17:44:33 +0200

    make private route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Header from './PhoneBook/Header/Header';
 import { getCurrentUser } from './redux/auth/authOperation';
+import PhoneBook from './PhoneBook/PhoneBook';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -20,6 +21,7 @@ const App = () => {
         <Route exact path={'/'} component={Home} />
         <Route path={'/login'} component={Login} />
         <Route path={'/register'} component={Register} />
+        <Route path={'/contacts'} component={PhoneBook} />
       </Switch>
     </>
   );
